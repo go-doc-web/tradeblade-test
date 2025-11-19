60ca6aabc4edebbb1b3bc6efc3c38796a3304766
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-11-19 11:32:42 +0200

    feat:create mobile menu & burger

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./Button.module.scss";
+
+/**
+
+ * @param {('primary'|'secondary'|'text')} variant
+ * @param {string} className
+ * @param {string} icon
+ * @param {React.ReactNode} children
+ * @param {object} props
+ */
+const Button = ({
+  children,
+  icon,
+  variant = "primary",
+  className,
+  ...props
+}) => {
+  const buttonClasses = clsx(styles.button, styles[variant], className);
+
+  let content = children;
+
+  if (icon) {
+    // 💡 Виправлено: застосовуємо клас styles.contentWrapper до div
+    content = (
+      <div className={styles.contentWrapper}>
+        <img src={icon} alt="Кнопка з іконкою" className={styles.icon} />
+
+        {children}
+      </div>
+    );
+  }
+
+  return (
+    <button className={buttonClasses} {...props}>
+      {content}
+    </button>
+  );
+};
+
+export default Button;
--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 import clsx from "clsx";
 import Logo from "../../components/logo/Logo";
 import NavLink from "../../components/nav-link";
+import Button from "../../components/button/Button";
 import styles from "./Header.module.scss";
 
 const navItems = [
@@ -13,6 +16,11 @@ const navItems = [
 ];
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   const containerClasses = clsx("container", styles.containerHeader);
   return (
     <header className={styles.header}>
@@ -31,9 +39,42 @@ const Header = () => {
           ))}
         </nav>
         <div className={styles.btnHeaderGroupe}>
-          <button className={styles.enterButton}>ВХОД</button>
-          <button className={styles.registerButton}>РЕГИСТРАЦИЯ</button>
+          <Button variant="secondary" className={styles.enterButton}>
+            ВХОД
+          </Button>
+          <Button variant="primary" className={styles.registerButton}>
+            РЕГИСТРАЦИЯ
+          </Button>
         </div>
+        <div
+          className={`${styles.burger} ${isOpen ? styles.open : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
+        {/* Mobile menu (виїжджаюча панель) */}
+        <nav className={`${styles.mobileMenu} ${isOpen ? styles.active : ""}`}>
+          {navItems.map((item) => (
+            <NavLink
+              key={item.name}
+              href={item.href}
+              onClick={toggleMenu}
+              isMobile={true}
+            >
+              {item.name}
+            </NavLink>
+          ))}
+          <div className={styles.btnsMobileMenu}>
+            <Button className={styles.mobileRegisterButton} variant="primary">
+              РЕГИСТРАЦИЯ
+            </Button>
+            <Button className={styles.mobileEnterButton} variant="secondary">
+              Войти
+            </Button>
+          </div>
+        </nav>
       </div>
     </header>
   );
