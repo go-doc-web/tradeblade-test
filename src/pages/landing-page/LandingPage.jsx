366974ec643dcb8934367e2c2d0e74b07c4bd2f2
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-11-20 02:27:53 +0200

    feat:create section numbers

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../sections/hero";
+import SectionNumbers from "../../sections/section-numbers/SectionNumbers";
 
 const LandingPage = () => {
   return (
     <>
       <Hero />
+      <SectionNumbers />
     </>
   );
 };
---
+++ src/sections/section-numbers/SectionNumbers.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import clsx from "clsx";
+import SectionTitle from "../../components/section-title";
+import styles from "./SectionNumbers.module.scss";
+
+const numbersItems = [
+  { id: "1", title: "Торговой прибыли", number: 2756, label: "%" },
+  {
+    id: "2",
+    title: `фьючерсных и спотовых сделок`,
+    number: 2756,
+    label: "",
+  },
+  {
+    id: "3",
+    title: `прибыль подписчиков`,
+
+    number: 2756,
+    label: "",
+  },
+];
+
+const SectionNumbers = () => {
+  return (
+    <section className={styles.sectionNumbers}>
+      <div className={clsx("container", styles.containerNumbers)}>
+        <div className={styles.numbersLeft}>
+          <SectionTitle
+            type="h2"
+            className={clsx("section-title", styles.title)}
+          >
+            Цифры
+          </SectionTitle>
+          <p className={styles.labelDate}>Cентябрь 2022</p>
+        </div>
+        <div className={styles.numbersRigth}>
+          <ul className={styles.listNumbers}>
+            {numbersItems.map((numberItem) => {
+              return (
+                <li key={numberItem.id} className={styles.itemNumbers}>
+                  <SectionTitle type="h3" className={styles.titleItem}>
+                    {numberItem.title}
+                  </SectionTitle>
+                  <span className={styles.number}>
+                    {numberItem.number}
+                    {numberItem.label ? numberItem.label : ""}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionNumbers;
